eba23eeaa968ab32ff63c1c2a474b93f45a0471c
ad.hanif <ad.hanif@outlook.com>
2023-07-23 20:39:34 +0200

    routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,14 +4,18 @@ import HeroSection from "./components/HeroSection";
 import SkillSection from "./components/SkillSection";
 import CtaSection from "./components/CtaSection";
 import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Navbar />
-      <HeroSection />
-      <SkillSection />
-      <CtaSection />
-      <Footer />
+
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route element={<SkillSection />} />
+        <Route element={<CtaSection />} />
+        <Route element={<Footer />} />
+      </Routes>
     </>
   );
 }
