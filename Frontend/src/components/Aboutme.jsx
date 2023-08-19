fe9b0ed84d4827f008dd5f579ff4d78b70788638
ad.hanif <ad.hanif@outlook.com>
2023-08-19 14:15:21 +0200

    typewriter-effect added

--- Frontend/src/components/Aboutme.jsx
+++ Frontend/src/components/Aboutme.jsx
@@ -2,21 +2,35 @@ import React from "react";
 import portrait from "../img/portrait.jpg";
 import JobCard from "./JobCard";
 import portfolioImage from "../img/portfolio.jpeg";
+import Typewriter from "typewriter-effect";
 
 export default function Aboutme() {
   return (
     <>
       <div
-        className=" relative overflow-hidden bg-fixed bg-no-repeat bg-center"
+        className="relative overflow-hidden  bg-no-repeat bg-right-bottom md:bg-center bg-cover  h-[300px] md:h-[700px]  md:bg-fixed"
         style={{
           backgroundImage: `url(${portfolioImage})`,
-          height: "800px",
         }}
       >
         <div
-          className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed bg-gradient-to-r from-black  "
-          // style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
-        ></div>
+          className="flex flex-col justify-center items-center absolute bottom-0 left-0 right-0 top-0  overflow-hidden bg-fixed bg-opacity-50   "
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
+        >
+          <h1 className="text-white font-medium text-4xl md:text-8xl text-center">
+            I am Adnan Hanif
+          </h1>
+          <span className="text-white mt-10 font-medium text-2xl md:text-5xl">
+            {" "}
+            <Typewriter
+              options={{
+                strings: ["Full-Stack Web-Developer", "Front-end Focus"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </div>
       </div>
 
       <div className="container flex flex-col  justify-between px-8   mx-auto mt-4  space-y-12 md:py-10  md:space-y-0">
