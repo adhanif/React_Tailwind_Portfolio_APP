f174f49843bb5ed3820a48595488436de94c811e
ad.hanif <ad.hanif@outlook.com>
2024-03-10 13:34:40 +0200

    fixed scroll to section

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Bars3Icon, BeakerIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import React, { useEffect, useState } from 'react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 export default function Navbar() {
   const [isDiplayed, setIsDiplayed] = useState(false);
@@ -13,50 +13,69 @@ export default function Navbar() {
     }
   };
 
-  window.addEventListener("scroll", changeNavBackground);
+  window.addEventListener('scroll', changeNavBackground);
 
-  const menuItems = ["Home", "About", "Skills", "Projects"];
+  const menuItems = [
+    { Home: 'home' },
+    { About: 'about' },
+    { Skills: 'skills' },
+    { Projects: 'projects' },
+  ];
 
   function handleClick() {
     setIsDiplayed(!isDiplayed);
   }
 
+  const handleScrollSection = (id) => {
+    const sectionElement = document.getElementById(id);
+    const offset = 90;
+    if (sectionElement) {
+      const targetScroll = sectionElement.offsetTop - offset;
+      sectionElement.scrollIntoView({ behavior: 'smooth' });
+      window.scrollTo({
+        top: targetScroll,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <>
       <nav
+        id='home'
         className={`fixed z-50 px-8 py-4 md:py-2 lg:py-4  text-white w-screen ${
           navbarColor
-            ? " bg-white text-black border-b-2  shadow-lg "
-            : "bg-transparent"
+            ? ' bg-white text-black border-b-2  shadow-lg '
+            : 'bg-transparent'
         }`}
       >
-        <div className="container mx-auto">
-          <div className="flex flex-col md:flex-row   justify-between md:items-center">
-            <div className="">
+        <div className='container mx-auto'>
+          <div className='flex flex-col md:flex-row   justify-between md:items-center'>
+            <div className=''>
               <h1
                 className={`font-bold text-xl lg:text-3xl ${
-                  navbarColor ? " text-black  " : " text-white"
+                  navbarColor ? ' text-black  ' : ' text-white'
                 }`}
               >
-                Adnan <span className="text-orange-500">Hanif</span>
+                Adnan <span className='text-orange-500'>Hanif</span>
               </h1>
             </div>
 
             {/* Menu icons  */}
             <div
               onClick={handleClick}
-              className="absolute right-8 cursor-pointer md:hidden "
+              className='absolute right-8 cursor-pointer md:hidden '
             >
               {isDiplayed ? (
                 <XMarkIcon
                   className={`h-7 w-7  ${
-                    navbarColor ? " text-black  " : " text-white"
+                    navbarColor ? ' text-black  ' : ' text-white'
                   }`}
                 />
               ) : (
                 <Bars3Icon
                   className={`h-7 w-7  ${
-                    navbarColor ? " text-black  " : " text-white"
+                    navbarColor ? ' text-black  ' : ' text-white'
                   }`}
                 />
               )}
@@ -64,28 +83,33 @@ export default function Navbar() {
 
             <div
               className={`flex flex-col md:flex-row md:space-x-6 lg:space-x-10 translate-all  Z-[-1] md:Z-auto duration-700 ease-in ${
-                !isDiplayed ? "top-12 hidden md:flex" : "top-[-490px]"
+                !isDiplayed ? 'top-12 hidden md:flex' : 'top-[-490px]'
               }`}
             >
               {menuItems.map((item, index) => {
+                const [key, value] = Object.entries(item)[0];
                 return (
                   <div
                     key={index}
-                    className="my-5 lg:my-0 md:pl-0 hover:scale-110"
+                    className='my-5 lg:my-0 md:pl-0 hover:scale-110'
                   >
                     <p
                       className={`"hover:text-orange-500 hover:scale-110 font-bold text-xl cursor-pointer ${
-                        navbarColor ? " text-black  " : " text-white"
+                        navbarColor ? ' text-black  ' : ' text-white'
                       }`}
+                      onClick={() => handleScrollSection(value)}
                     >
-                      {item}
+                      {key}
                     </p>
                   </div>
                 );
               })}
             </div>
-            <button className="hidden   md:block p-2 px-4  text-white bg-red-900 hover:bg-orange-500  rounded-md text-center  hover:bg-brightRedLigh focus:outline-none hover:scale-110">
-              {" "}
+            <button
+              className='hidden   md:block p-2 px-4  text-white bg-red-900 hover:bg-orange-500  rounded-md text-center  hover:bg-brightRedLigh focus:outline-none hover:scale-110'
+              onClick={() => handleScrollSection('contact')}
+            >
+              {' '}
               Contact me
             </button>
           </div>
--- Frontend/src/components/SkillSection.jsx
+++ Frontend/src/components/SkillSection.jsx
@@ -1,80 +1,80 @@
-import React from "react";
-import html5 from "../img/html5.svg";
-import css3 from "../img/css3.svg";
-import javascript from "../img/javascript-1.svg";
-import react from "../img/react.svg";
-import expressJS from "../img/expressJS.svg";
-import tailwindcss from "../img/tailwindcss.svg";
-import bootstrap from "../img/bootstrap.svg";
-import materialUI from "../img/material-ui.svg";
-import mongodb from "../img/mongodb.svg";
-import SkillsCard from "./SkillsCard";
-import nodeJs from "../img/nodejs.svg";
-import sql from "../img/sql.svg";
-import python from "../img/python.svg";
-import git from "../img/git.svg";
-import figma from "../img/figma.svg";
-import { AnimationOnScroll } from "react-animation-on-scroll";
+import React from 'react';
+import html5 from '../img/html5.svg';
+import css3 from '../img/css3.svg';
+import javascript from '../img/javascript-1.svg';
+import react from '../img/react.svg';
+import expressJS from '../img/expressJS.svg';
+import tailwindcss from '../img/tailwindcss.svg';
+import bootstrap from '../img/bootstrap.svg';
+import materialUI from '../img/material-ui.svg';
+import mongodb from '../img/mongodb.svg';
+import SkillsCard from './SkillsCard';
+import nodeJs from '../img/nodejs.svg';
+import sql from '../img/sql.svg';
+import python from '../img/python.svg';
+import git from '../img/git.svg';
+import figma from '../img/figma.svg';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 export default function SkillSection() {
   return (
-    <>
-      <h1 className="mt-10 md:mt-20 font-bold text-3xl md:text-6xl mb-2 text-center underline underline-offset-8 decoration-red-900 ">
+    <div id='skills'>
+      <h1 className='mt-10 md:mt-20 font-bold text-3xl md:text-6xl mb-2 text-center underline underline-offset-8 decoration-red-900 '>
         Skills
       </h1>
-      <div className="container grid grid-cols-6 gap-10   justify-items-center md:px-8 mx-auto mt-20 mb-20 lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 ">
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={html5} heading={"HTML5"} />
+      <div className='container grid grid-cols-6 gap-10   justify-items-center md:px-8 mx-auto mt-20 mb-20 lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 '>
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={html5} heading={'HTML5'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={css3} heading={"CSS3"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={css3} heading={'CSS3'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={javascript} heading={"JAVASCRIPT"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={javascript} heading={'JAVASCRIPT'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={python} heading={"PYTHON"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={python} heading={'PYTHON'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={react} heading={"REACT"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={react} heading={'REACT'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={expressJS} heading={"EXPRESS.JS"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={expressJS} heading={'EXPRESS.JS'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={nodeJs} heading={"NODE.JS"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={nodeJs} heading={'NODE.JS'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={tailwindcss} heading={"TAILWINDCSS"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={tailwindcss} heading={'TAILWINDCSS'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={bootstrap} heading={"BOOTSTRAP"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={bootstrap} heading={'BOOTSTRAP'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={materialUI} heading={"MATERIAL UI"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={materialUI} heading={'MATERIAL UI'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={mongodb} heading={"MONGODB"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={mongodb} heading={'MONGODB'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={sql} heading={"SQL"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={sql} heading={'SQL'} />
         </AnimationOnScroll>
 
-        <AnimationOnScroll animateIn="animate__fadeInUp" duration={1}>
-          <SkillsCard logo={figma} heading={"FIGMA"} />
+        <AnimationOnScroll animateIn='animate__fadeInUp' duration={1}>
+          <SkillsCard logo={figma} heading={'FIGMA'} />
         </AnimationOnScroll>
       </div>
-    </>
+    </div>
   );
 }
